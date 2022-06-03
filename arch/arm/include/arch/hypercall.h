9b45fcb17654d62d7faa48bba551c1fabf47c46c
Christopher Clark <christopher.clark@starlab.io>
2022-06-03 22:46:51 +0000

    arm32: implement hypercalls in hypercall.h
    TODO: check the 5 arg hypercall and arg passing for C

--- arch/arm/include/arch/hypercall.h
+++ arch/arm/include/arch/hypercall.h
@@ -8,28 +8,63 @@
 
 #include <xtf/lib.h>
 
-#define _hypercall_1(type, hcall, a1)                   \
-    ({                                                  \
-        UNIMPLEMENTED();                                \
-        (type)0;                                        \
+#define _hypercall_1(type, hcall, A0)                   \
+    ({                                                         \
+    register unsigned long nr asm ("r12") = hcall;             \
+    register unsigned long a0 asm ("r0") = (unsigned long) A0; \
+                                                               \
+    asm volatile ("hvc #0xea1"                                 \
+                  : [nr] "+r" (nr), [a0] "+r" (a0)             \
+                  :                                            \
+                  : "memory");                                 \
+        (type)a0;                                              \
     })
 
-#define _hypercall_2(type, hcall, a1, a2)               \
-    ({                                                  \
-        UNIMPLEMENTED();                                \
-        (type)0;                                        \
+#define _hypercall_2(type, hcall, A0, A1)               \
+    ({                                                         \
+    register unsigned long nr asm ("r12") = hcall;             \
+    register unsigned long a0 asm ("r0") = (unsigned long) A0; \
+    register unsigned long a1 asm ("r1") = (unsigned long) A1; \
+                                                               \
+    asm volatile ("hvc #0xea1"                                 \
+                  : [nr] "+r" (nr), [a0] "+r" (a0),            \
+                    [a1] "+r" (a1)                             \
+                  :                                            \
+                  : "memory");                                 \
+        (type)a0;                                              \
     })
 
-#define _hypercall_3(type, hcall, a1, a2, a3)           \
-    ({                                                  \
-        UNIMPLEMENTED();                                \
-        (type)0;                                        \
+#define _hypercall_3(type, hcall, A0, A1, A2)                  \
+    ({                                                         \
+    register unsigned long nr asm ("r12") = hcall;             \
+    register unsigned long a0 asm ("r0") = (unsigned long) A0; \
+    register unsigned long a1 asm ("r1") = (unsigned long) A1; \
+    register unsigned long a2 asm ("r2") = (unsigned long) A2; \
+                                                               \
+    asm volatile ("hvc #0xea1"                                 \
+                  : [nr] "+r" (nr), [a0] "+r" (a0),            \
+                    [a1] "+r" (a1), [a2] "+r" (a2)             \
+                  :                                            \
+                  : "memory");                                 \
+        (type)a0;                                              \
     })
 
-#define _hypercall_5(type, hcall, a1, a2, a3, a4, a5)  \
-    ({                                                 \
-        UNIMPLEMENTED();                               \
-        (type)0;                                       \
+#define _hypercall_5(type, hcall, A0, A1, A2, A3, A4)          \
+    ({                                                         \
+    register unsigned long nr asm ("r12") = hcall;             \
+    register unsigned long a0 asm ("r0") = (unsigned long) A0; \
+    register unsigned long a1 asm ("r1") = (unsigned long) A1; \
+    register unsigned long a2 asm ("r2") = (unsigned long) A2; \
+    register unsigned long a3 asm ("r3") = (unsigned long) A3; \
+    register unsigned long a4 asm ("r4") = (unsigned long) A4; \
+                                                               \
+    asm volatile ("hvc #0xea1"                                 \
+                  : [nr] "+r" (nr), [a0] "+r" (a0),            \
+                    [a1] "+r" (a1), [a2] "+r" (a2),            \
+                    [a3] "+r" (a3), [a4] "+r" (a4)             \
+                  :                                            \
+                  : "memory");                                 \
+        (type)a0;                                              \
     })
 
 #endif /* XTF_ARM_HYPERCALL_H */
