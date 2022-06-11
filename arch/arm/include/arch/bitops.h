d92a3803fca31cfc26911e8cc3564a68868bb625
Christopher Clark <christopher.clark@starlab.io>
2022-06-11 23:49:34 +0000

    implement remaining bitops

--- arch/arm/include/arch/bitops.h
+++ arch/arm/include/arch/bitops.h
@@ -10,20 +10,55 @@
 
 static inline bool test_bit(unsigned int bit, const void *addr)
 {
-    UNIMPLEMENTED();
-    return false;
+    unsigned long mask = (1 << bit);
+
+    asm volatile ( "dmb ish" : : : );
+
+    return ACCESS_ONCE(*(unsigned long *)addr) & mask;
 }
 
 static inline bool test_and_set_bit(unsigned int bit, volatile void *addr)
 {
-    UNIMPLEMENTED();
-    return false;
+    register unsigned long val, oldval, ok;
+    register unsigned long mask = (1 << bit);
+    register unsigned long *p = (unsigned long *)addr;
+
+    asm volatile (
+        "1:\n"
+        "dmb ish\n"
+        "ldrex %1, [%4]\n"
+        "orr %0, %1, %5\n"
+        "strex %3, %0, [%4]\n"
+        "cmp %3, #0\n"
+        "bne 1b\n"
+        "dmb ish\n"
+        : "=&r" (val), "=&r" (oldval), "+m" (*p), "=&r" (ok)
+        : "r" (p), "r" (mask)
+        : "memory");
+
+    return oldval & mask;
 }
 
 static inline bool test_and_change_bit(unsigned int bit, volatile void *addr)
 {
-    UNIMPLEMENTED();
-    return false;
+    register unsigned long val, oldval, ok;
+    register unsigned long mask = (1 << bit);
+    register unsigned long *p = (unsigned long *)addr;
+
+    asm volatile (
+        "1:\n"
+        "dmb ish\n"
+        "ldrex %1, [%4]\n"
+        "eor %0, %1, %5\n"
+        "strex %3, %0, [%4]\n"
+        "cmp %3, #0\n"
+        "bne 1b\n"
+        "dmb ish\n"
+        : "=&r" (val), "=&r" (oldval), "+m" (*p), "=&r" (ok)
+        : "r" (p), "r" (mask)
+        : "memory");
+
+    return oldval & mask;
 }
 
 static inline bool test_and_clear_bit(unsigned int bit, volatile void *addr)
